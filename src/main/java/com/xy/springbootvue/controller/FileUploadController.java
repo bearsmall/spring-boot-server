7bf18585d0d427395c2f6c36542868463650740a
bearsmall <bearsmall@bupt.edu.cn>
2018-12-10 15:20:12 +0800

    'upload_dir_optimized'

--- src/main/java/com/xy/springbootvue/controller/FileUploadController.java
+++ src/main/java/com/xy/springbootvue/controller/FileUploadController.java
@@ -1,5 +1,6 @@
 package com.xy.springbootvue.controller;
 
+import com.xy.springbootvue.util.ZipUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +15,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Objects;
 
 @RestController
@@ -21,8 +25,10 @@ import java.util.Objects;
 @CrossOrigin
 public class FileUploadController {
 
-    @Value("${prop.upload-folder}")
-    private String UPLOAD_FOLDER;
+    @Value("${prop.upload-zip-folder}")
+    private String UPLOAD_ZIP_FOLDER;
+    @Value("${prop.upload-unzip-folder}")
+    private String UPLOAD_UNZIP_FOLDER;
     private Logger logger = LoggerFactory.getLogger(FileUploadController.class);
 
     @PostMapping("/singlefile")
@@ -34,19 +40,44 @@ public class FileUploadController {
             return code;
         }
         try {
+            //TODO
+            //待优化
             byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOAD_FOLDER + File.separator+file.getOriginalFilename());
+
+            //组织文件上传、解压目录及路径（采用的目录结构为：年/月/日/文件 的形式
+            Calendar now = Calendar.getInstance();
+            Date date = new Date(now.getTimeInMillis());
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+            int year = now.get(Calendar.YEAR);
+            int month = now.get(Calendar.MONTH) + 1;
+            int day = now.get(Calendar.DAY_OF_MONTH);
+            String fileDir = year+File.separator+month+File.separator+day;
+            String storeFileName = simpleDateFormat.format(date)+"-"+file.getOriginalFilename();
+
+            //构造上传地址、压缩地址
+            String zipPath = UPLOAD_ZIP_FOLDER + File.separator+fileDir+File.separator+storeFileName;
+            String unZipPath = UPLOAD_UNZIP_FOLDER + File.separator+fileDir+File.separator+storeFileName.substring(0,storeFileName.indexOf("."));
+
+            //目录构造
+            Path path = Paths.get(zipPath);
             if (!Files.isWritable(path)) {
-                Files.createDirectories(Paths.get(UPLOAD_FOLDER));
+                Files.createDirectories(Paths.get(UPLOAD_ZIP_FOLDER + File.separator+fileDir));
             }
+            //写入磁盘
             Files.write(path, bytes);
+            //如果上传压缩包则进行解压
+            if(storeFileName.endsWith("zip")) {
+                ZipUtils.decompress(zipPath, unZipPath);
+            }
             code = "file upload success";
             logger.debug(code);
-            return code;
         } catch (IOException e) {
             code = e.getMessage();
             logger.error(code);
-            return code;
+        } catch (Exception e) {
+            code = e.getMessage();
+            logger.error(code);
         }
+        return code;
     }
 }
